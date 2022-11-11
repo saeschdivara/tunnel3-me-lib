c253a9e2006ce75747895d54b727f41abfd62ff4
Sascha Häusler <sascha.haeusler@bitvoodoo.ch>
2022-11-11 10:43:51 +0100

    Add post install download

---
+++ postinstall.js
@@ -0,0 +1,45 @@
+const os = require('os');
+const http = require('https');
+const fs = require('fs');
+
+const version = 'v0.0.6'
+const baseUrl = 'https://github.com/saeschdivara/tunnel3-me-client/releases/download/' + version + '/'
+
+let osPrefix;
+
+if (os.platform() === 'linux') {
+  osPrefix = 'ubuntu'
+}
+else if (os.platform() === 'darwin') {
+  osPrefix = 'mac'
+}
+else if (os.platform() === 'win32') {
+  osPrefix = 'windows.exe'
+}
+
+const binaryName = 'tunnel3MeLocal_' + osPrefix
+const binaryPath = 'bin/' + binaryName
+
+if (!fs.existsSync('bin')) {
+  fs.mkdirSync('bin')
+}
+
+const file = fs.createWriteStream(binaryPath);
+
+http.get(baseUrl + binaryName, function(redirectResponse) {
+  // follow Github redirect
+  if (redirectResponse.statusCode === 302) {
+    http.get(redirectResponse.headers.location, function(response) {
+
+      response.pipe(file);
+
+      // after download completed close filestream
+      file.on("finish", () => {
+        file.close();
+        console.log("Download Completed");
+
+        fs.chmodSync(binaryPath, 0o0755);
+      });
+    });
+  }
+});
